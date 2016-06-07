ed5e1d714eb05263dcb0e9478b47986ca06e08e5
Ken Haase <kh@beingmeta.com>
2016-06-06 20:19:05 -0400

    Remove role cleverness which required credentials (EC2) to get credentials

--- aws/roles.scm
+++ aws/roles.scm
@@ -103,9 +103,7 @@
 	     (lambda (var (value))
 	       (if (bound? value)
 		   (and (not (equal? value aws/role))
-			(begin (if (ec2/role! value) (set! aws/role value) 
-				   (begin (logwarn |RoleFailed| "Couldn't set EC2 role to " value)
-				     (ec2/role! #f)))
+			(begin (when (ec2/role! value) (set! aws/role value))
 			  #t))
 		   aws/role)))
 
