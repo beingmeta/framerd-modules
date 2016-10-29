0fc3f6aa40b57a51cb457cc1eed0c3aac5ad6046
Ken Haase <kh@beingmeta.com>
2016-10-29 13:19:22 -0400

    Fixes to adjslots, mgo/mod functions, other small tweaks

--- mongodb/pools.scm
+++ mongodb/pools.scm
@@ -17,22 +17,24 @@
 (define (fetchfn oid/s collection)
   (if (vector? oid/s)
       (let ((values (mongodb/find collection
-				  `#[_id #[$oneof ,(elts oid/s)]]))
+			`#[_id #[$oneof ,(elts oid/s)]
+			   #[__index 0]]))
 	    (result (make-vector (length oid/s) #f))
 	    (map (make-hashtable)))
 	(do-choices (value values) (store! map (get value '_id) value))
 	(doseq (oid oid/s i)
 	  (vector-set! result i (get map oid)))
 	result)
-      (mongodb/get collection oid/s)))
+      (mongodb/get collection oid/s #[return #[__index 0]])))
 
 (define (mgo/pool/fetch oid/s collection) (fetchfn oid/s collection))
 
 (define (allocfn n collection)
   (if (and (integer? n) (> n 0) (<= n 1024))
       (let* ((mod (mongodb/modify collection
-				  #[_id "_pool"] 
-				  `#[$inc #[load ,n]]))
+		      #[_id "_pool"] 
+		    `#[$inc #[load ,n]]
+		    #[original #t]))
 	     (before (get mod 'value))
 	     (start (get before 'load))
 	     (base (get before 'base))
@@ -40,7 +42,8 @@
 	(dotimes (i n)
 	  (set+! result (oid-plus base (+ i start))))
 	(do-choices (oid result)
-	  (mongodb/insert! collection `#[_id ,oid]))
+	  (mongodb/insert! collection `#[_id ,oid])
+	  (set-oid-value! oid `#[_id ,oid]))
 	result)
       (irritant n |BadAllocCount| "For pool in " collection)))
 
@@ -58,7 +61,7 @@
 				  mgo/pool/fetch 
 				  #f ;; save
 				  #f ;; lock
-				  #f ;; alloc (mgo/pool/alloc)
+				  mgo/pool/alloc ;; alloc (mgo/pool/alloc)
 				  collection ;; state
 				  #t)))
 	 (store! pool-table pool collection)
@@ -76,8 +79,21 @@
       (irritant oid |No pool| mgo/store!)
       (if (fail? collection)
 	  (irritant pool |Not A MongoDB pool| mgo/store!)
-	  (mongodb/update! collection `#[_id ,oid]
-			   #[$set #[,slotid ,values]]))))
+	  (update!
+	   (mongodb/modify! collection 
+	       `#[_id ,(if (ambiguous? oid)
+			   `#[$oneof ,oid]
+			   oid)]
+	     `#[$set (if (ambiguous? slotid)
+			 (get-store-modifier slotid values)
+			 #[,slotid ,values])]
+	     #[new #t return #[__index 0]])))))
+
+(defambda (get-store-modifier slotids values (result))
+  (set! result #[])
+  (do-choices (slotid slotids)
+    (store! result slotid values))
+  result)
 
 (defambda (mgo/add! oid slotid values (pool) (collection))
   (set! pool (getpool oid))
@@ -86,8 +102,21 @@
       (irritant oid |No pool| mgo/store!)
       (if (fail? collection)
 	  (irritant pool |Not A MongoDB pool| mgo/store!)
-	  (mongodb/update! collection `#[_id ,oid]
-			   #[$addToSet #[,slotid ,values]]))))
+	  (update! (mongodb/modify! collection 
+		       (if (ambiguous? oid)
+			   `#[_id #[$in ,oid]]
+			   `#[_id ,oid])
+		     `#[$addToSet ,(get-add-modifier slotid values)]
+		     #[new #t return #[__index 0]])))))
+
+(defambda (get-add-modifier slotids values)
+  (if (unique? slotids)
+      `#[,slotids ,(if (unique? values) values `#[$each ,values])]
+      (let ((q (frame-create #f)))
+	(do-choices (slotid slotids)
+	  (add! q slotid 
+		(if (unique? values) values `#[$each ,values]))
+	  q))))
 
 (defambda (mgo/drop! oid slotid (values) (pool) (collection))
   (set! pool (getpool oid))
@@ -96,14 +125,37 @@
       (irritant oid |No pool| mgo/store!)
       (if (fail? collection)
 	  (irritant pool |Not A MongoDB pool| mgo/store!)
-	  (if (bound? values)
-	      (mongodb/update! collection `#[_id ,oid]
-			       #[$pull #[,slotid ,values]])
-	      (mongodb/update! collection `#[_id ,oid]
-			       #[$uset #[,slotid 1]])))))
+	  (update!
+	   (if (bound? values)
+	       (mongodb/modify! collection `#[_id ,oid]
+		 (if (unique? values)
+		     `#[$pull ,(if (unique? slotid)
+				   `#[,slotid ,values]
+				   (get-store-modifier slotid values))]
+		     `#[$pullAll ,(if (unique? slotid)
+				      `#[,slotid ,values]
+				      (get-store-modifier slotid values)
+				      )])
+		 #[new #t return #[__index 0]])
+	       (mongodb/modify! collection 
+		   `#[_id ,oid] (if (ambiguous? slotid)
+				    (get-drop-all-modifier slotid)
+				    `#[$unset #[,slotid 1]])
+		   #[new #t return #[__index 0]]))))))
+
+(define (get-drop-all-modifier slotids (result #[]))
+  (do-choices (slotid slotids)
+    (store! result slotid 1))
+  result)
 
 (define (mgo/decache! oid (slotid #f))
   (swapout oid))
+
+(define (update! result (value #f))
+  (when (test result 'value)
+    (set! value (get result 'value))
+    (set-oid-value! (get value '_id) value))
+  (or value result))
 
 ;;; Defining adjunct slots of various kinds
 
@@ -130,7 +182,7 @@
   (let* ((fetchfn (lambda (oid collection)
 		    (if extract
 			(get (mongodb/find collection (adjunct-query query oid)
-					   `#[returns ,extract])
+			       `#[returns ,extract])
 			     extract)
 			(mongodb/find collection (adjunct-query query oid)))))
 	 (coll (get pool-table pool))
@@ -139,17 +191,17 @@
 	      (glom (mongodb/name coll) "/" (collection/name coll) "/" slot)
 	      (glom (pool-id pool) "/" slot)))
 	 (adjunct (cons-extindex name fetchfn #f qcoll #t)))
-      (info%watch "MAKE-ADJSLOT/setup" adjunct name coll fetchfn)
-      (store! adjunct-indices (vector pool slot qcoll query extract) adjunct)
-      (store! adjunct-indices (vector pool slot) 
-	      (vector pool slot qcoll query extract))
-      (use-adjunct adjunct slot pool)
-      adjunct))
+    (info%watch "MAKE-ADJSLOT/setup" adjunct name coll fetchfn)
+    (store! adjunct-indices (vector pool slot qcoll query extract) adjunct)
+    (store! adjunct-indices (vector pool slot) 
+	    (vector pool slot qcoll query extract))
+    (use-adjunct adjunct slot pool)
+    adjunct))
 
 (defambda (adjunct-query query subjects)
   (cond ((fail? subjects) query)
 	((ambiguous? query)
-	 (for-choices (q query) (adjslot-query q subjects)))
+	 (for-choices (q query) (adjunct-query q subjects)))
 	((or (oid? query) (symbol? query) (number? query) (string? query))
 	 query)
 	((or (slotmap? query) (schemap? query))
@@ -162,25 +214,25 @@
 			   (if (ambiguous? subjects)
 			       `#[$oneof ,subjects]
 			       subjects))
-		   (if (and (singleton? v)
+		   (if (and (unique? v)
 			    (or (oid? v) (symbol? v) (number? v) (string? v)))
 		       (store! copy key v)
-		       (store! copy key (adjslot-query v subjects))))))
+		       (store! copy key (adjunct-query v subjects))))))
 	   copy))
-	((vector? query) (map (lambda (e) (adjslot-query e subjects)) query))
+	((vector? query) (map (lambda (e) (adjunct-query e subjects)) query))
 	((pair? query) 
-	 (cons (adjslot-query (car e) subjects)
-	       (adjslot-query (cdr e) subjects)))
+	 (cons (adjunct-query (car query) subjects)
+	       (adjunct-query (cdr query) subjects)))
 	((mongovec? query)
 	 (let ((unpacked (unpack-compound query)))
 	   (vector->compound
-	    (map (lambda (e) (adjslot-query e subjects)) (cdr unpacked))
+	    (map (lambda (e) (adjunct-query e subjects)) (cdr unpacked))
 	    '%mongovec
 	    #f #f)))
 	((mongomap? query)
 	 (let ((unpacked (unpack-compound query)))
 	   (vector->compound
-	    (map (lambda (e) (adjslot-query e subjects)) (cdr unpacked))
+	    (map (lambda (e) (adjunct-query e subjects)) (cdr unpacked))
 	    '%mongomap
 	    #f #f)))
 	(else query)))
@@ -195,12 +247,7 @@
 	((and (not info) (not (oid? base)))
 	 (irritant base |InvalidPoolBase| "For collection " collection))
 	((not info)
-	 (mongodb/insert! collection
-			  `#[_id "_pool" base ,base capacity ,cap
-			     name ,(collection/name collection)
-			     initialized ,(gmtimestamp 'seconds)
-			     init ,(config 'sessionid)
-			     load ,(get-init-load collection base)])
+	 (init-pool collection base cap)
 	 base)
 	((not (test info 'base))
 	 (mongodb/update! collection #[_id "_pool"] `#[$set #[base ,base]])
@@ -219,14 +266,12 @@
 				   (> cap 0)
 				   (<= cap (* 4 1000 1000 1000)))))
 	 (irritant cap |InvalidPoolCapacity| "For collection " collection))
-	((not info) 
-	 (mongodb/insert! collection
-			  `#[_id "_pool" base ,base capacity ,cap
-			     name ,(collection/name collection)
-			     load ,(get-init-load collection base)])
+	((not info)
+	 (init-pool collection base cap)
 	 cap)
 	((not (test info 'capacity))
-	 (mongodb/update! collection #[_id "_pool"] `#[$set #[capacity ,cap]])
+	 (mongodb/update! collection #[_id "_pool"]
+	   `#[$set #[capacity ,cap]])
 	 cap)
 	((test info 'capacity cap) cap)
 	(else (irritant base |InconsistentCapacity| 
@@ -234,9 +279,15 @@
 			" in " info
 			"for " collection))))
 
+(define (init-pool collection base cap)
+  (mongodb/insert! collection
+    `#[_id "_pool" base ,base capacity ,cap
+       name ,(collection/name collection)
+       load ,(get-init-load collection base)]))
+
 (define (get-max-id collection)
   (largest (get (mongodb/find collection #[_id #[$type "objectId"]]
-			      #[return #[_id: 1]])
+		  #[return #[_id: 1]])
 		'_id)))
 (define (get-init-load collection base)
   (1+ (try (oid-offset (get-max-id collection) base) 0)))
