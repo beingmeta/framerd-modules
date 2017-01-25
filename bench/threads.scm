0946c60b6709c86c964a0c4a673379cd1e0379f5
Ken Haase <kh@beingmeta.com>
2017-01-25 12:08:35 -0500

    Improve reporting, fix some bugs

--- bench/threads.scm
+++ bench/threads.scm
@@ -3,7 +3,7 @@
 
 (in-module 'bench/threads)
 
-(use-module '{logger varconfig mttools stringfmts})
+(use-module '{logger varconfig mttools optimize stringfmts})
 
 (module-export! '{tbench})
 
@@ -15,39 +15,42 @@
 	(threadcount (mt/threadcount nthreads))
 	;; Run these once here to catch any superficial bugs before
 	;; the benchmark begins
-	(thread-data-test (and per-thread (per-thread)))
-	(looptest (apply per-thread thread-data args))
-	(threads {}))
-    (let ((loopfn (lambda (threadno thread-data)
+	(looptest (apply proc (and per-thread (per-thread)) args)))
+    (when (config 'optimize #f)
+      (optimize-procedure! proc)) ;; { loopfn} Can't optimize a lexically bound procedure
+    (let ((loopfn (lambda (threadno thread-data (count 0))
 		    (until (past? end)
-		      (apply proc thread-data args))))
-	  (before (rusage)))
+		      (apply proc thread-data args)
+		      (set! count (1+ count)))
+		    count))
+	  (before (rusage))
+	  (thread-results #f))
       (logwarn |ThreadBenchStart| 
-	(print-slots before '#(pid load resident memusage)))
-      (dotimes (i nthreads)
-	(set+! threads 
-	       (threadcall loopfn i (and per-thread (per-thread)))))
+	"With " (or threadcount "no") " threads for " (secs->string (difftime end))
+	" load=" (get before 'load) " mem=" (get before 'memusage))
+      (if threadcount
+	  (let ((threads {}))
+	    (dotimes (i threadcount)
+	      (set+! threads (threadcall loopfn i (and per-thread (per-thread)))))
+	    (threadjoin threads)
+	    (set! thread-results (map thread/result (choice->vector threads))))
+	  (set! thread-results (vector (loopfn #f (and per-thread (per-thread))))))
       (let ((after (rusage)))
-	(let* ((clocktime (- (get after 'elapsed) (get before 'elapsed)))
+	(let* ((clocktime (- (get after 'clock) (get before 'clock)))
 	       (stime (- (get after 'stime) (get before 'stime)))
 	       (utime (- (get after 'utime) (get before 'utime)))
 	       (cputime (+ stime utime))
-	       (memdiff (- (get after 'memusage) (get before 'memusage))))
-	  (logwarn |ThreadBenchFinished| 
-	    "Elapsed: " (secs->string clocktime) 
-	    ", cputime=" (secs->string cputime)
-	    ", utime=" (secs->string utime)
-	    ", stime=" (secs->string stime) "\n"
-	    "    load=" (get after 'load) ", avg="
-	    " " (first (get after 'loadavg))  
-	    " " (second (get after 'loadavg))
-	    " " (third (get after 'loadavg)) "\n    "
-	    (print-slots after '#(memusage resident)))
-	  (logwarn |ThreadBenchSummary|
-	    "CPU=" (show% cputime clocktime) 
+	       (memdiff (- (get after 'memusage) (get before 'memusage)))
+	       (total-calls (reduce + thread-results 0)))
+	  (logwarn |Finished| 
+	    (printnum total-calls) " calls in " (secs->string clocktime) " "
+	    "(" (printnum (/~ total-calls clocktime) 2) " calls/sec) "
+	    "using " (secs->string cputime) " cputime")
+	  (logwarn |CPU%|
+	    (show% cputime clocktime) 
 	    (when (and nthreads (> nthreads 1))
 	      (printout " (" 
-		(show% (/ cputime nthreads) clocktime)
+		(show% (/ cputime threadcount) clocktime)
 		" * " nthreads ") "))
 	    " memdiff=" (bytesize memdiff)))))))
 
