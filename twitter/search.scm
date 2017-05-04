55f5623c6a9d21e51e49f80882923aeae8997000
Ken Haase <kh@beingmeta.com>
2017-05-04 14:12:20 -0400

    Cleaned up search API

--- twitter/search.scm
+++ twitter/search.scm
@@ -4,10 +4,13 @@
 (in-module 'twitter/search)
 
 (use-module '{oauth varconfig logger})
+(use-module '{twitter})
 
 (module-export! '{twitter/searchapi
 		  twitter/search
 		  twitter/search/n})
+
+(define %loglevel %warn%)
 
 ;;(load-config (get-component "listenbot.cfg"))
 
@@ -18,40 +21,84 @@
 
 (define (getid x) (get x 'id))
 
-(define (twitter/search q (blocksize) (creds #f))
-  (default! blocksize 
-    (if (table? q) 
-	(getopt q 'count (getopt q 'blocksize 5000))
-	5000))
+(define (twitter/search q (opts #f) (fetchsize) (creds #f))
+  (when (fixnum? opts)
+    (set! fetchsize opts)
+    (set! opts #[]))
+  (default! fetchsize
+    (if (table? q)
+	(getopt q "count" (getopt opts 'fetchsize 100))
+	(getopt opts 'fetchsize 100)))
   (when (string? q)
-    (set! q (frame-create "q" q "count" blocksize)))
-  (let* ((creds (if (and creds (pair? creds)) creds
+    (set! q (frame-create #f "q" q "count" fetchsize)))
+  (let* ((start (elapsed-time))
+	 (creds (if (and creds (pair? creds)) creds
 		    (try (get q 'creds) (twitter/creds))))
 	 (r (oauth/call creds 'GET search-endpoint (try (get q 'next) q)))
 	 (tweets (get r 'statuses))
-	 (min_id (smallest (get (elts tweets) 'id))))
-    `#[tweets ,(get r 'statuses) q ,q count ,blocksize
-       creds ,creds
-       next #["q" ,(get q "q") "count" ,blocksize
+	 (ids (get (elts tweets) 'id))
+	 (min_id (smallest ids))
+	 (max_id (largest ids)))
+    (lognotice |Twitter/search| 
+      "Got " (length tweets) " tweets for " (write (getopt q "q")) 
+      " in " (secs->string (elapsed-time start)) " based on:\n"
+      (pprint q))
+    `#[q ,q count ,fetchsize
+       creds ,creds min_id ,min_id max_id ,max_id
+       tweets ,(get r 'statuses)
+       %original ,(get r '%original)
+       next #["q" ,(get q "q") "count" ,fetchsize
 	      "max_id" ,(-1+ min_id)]]))
 
-(define (twitter/search/n q n (blocksize) (creds (twitter/creds)))
-  (default! blocksize (quotient n 10))
-  (let ((blocks '())
-	(done #f)
-	(count 0)
-	(result (oauth/call creds 'GET search-endpoint
-			    `#["q" ,q "count" ,blocksize])))
+(define (twitter/search/n q (opts #f) (n) (blocksize) (creds (twitter/creds)))
+  (when (fixnum? opts) (set! n opts) (set! opts #f))
+  (default! n 
+    (try (getopt opts 'count)
+	 (tryif (table? q) (get q "count"))
+	 100))
+  (default! blocksize
+    (getopt opts 'blocksize (min (quotient n 10) n 100)))
+  (when (string? q) (set! q `#["q" ,q "count" ,blocksize]))
+  (let* ((start (elapsed-time))
+	 (start_min (getopt opts 'min_id (try (get q "min_id") #f)))
+	 (start_max (getopt opts 'max_id (try (get q "max_id") #f)))
+	 (qstring (get q "q"))
+	 (backward (or (< n 0) (< blocksize 0) start_max))
+	 (result #f)
+	 (blocks '())
+	 (done #f)
+	 (count 0))
+    (if backward
+	(when start_max (store! q "max_id" start_max))
+	(when start_min (store! q "min_id" start_min)))
+    (set! result 
+	  (oauth/call creds 'GET search-endpoint
+		      (if (table? q) q `#["q" ,qstring "count" ,blocksize])))
     (while (and (not done) (< count n) (exists? result) (table? result))
       (let* ((tweets (and (exists? result) result (table? result)
 			  (test result 'statuses)
 			  (get result 'statuses)))
 	     (metadata (get result 'search_metadata))
-	     (min_id (smallest (elts (map (lambda (x) (get x 'id)) tweets)))))
-	(logwarn |GotTweets| "Got " (length tweets) " tweets, total=" (+ count (length blocks)))
-	(set! blocks (cons tweets blocks))
+	     (ids (elts (map (lambda (x) (get x 'id)) tweets)))
+	     (min_id (smallest ids))
+	     (max_id (largest ids)))
+	(if (= (length tweets) 0)
+	    (set! done #t)
+	    (set! blocks (cons tweets blocks)))
 	(set! count (+ count (length tweets)))
-	(set! result (oauth/call creds 'GET search-endpoint
-				 `#["q" ,q "count" ,blocksize
-				    "max_id" ,(-1+ min_id)]))))
+	(unless done
+	  (set! result
+		(oauth/call creds 'GET search-endpoint
+			    `#["q" ,q "count" ,blocksize
+			       ,(if backward "max_id" "min_id")
+			       ,(if backward (-1+ min_id) (1+ max_id))])))))
+    (lognotice |Twitter/Search/N| 
+      "Got " count "/" n " tweets for " (write qstring) 
+      " in " (secs->string (elapsed-time start)))
     (apply append blocks)))
+
+
+
+
+
+
