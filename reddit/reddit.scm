49dfe045d976bcb36b919b4ed3fde12bd582ee40
Ken Haase <kh@beingmeta.com>
2017-06-13 20:10:21 -0400

    Add reddit/creds and more configurability of reddit/thing

--- reddit/reddit.scm
+++ reddit/reddit.scm
@@ -6,12 +6,29 @@
 (use-module '{fdweb texttools reflection varconfig logger})
 (use-module '{oauth})
 
-(module-export! '{reddit-key reddit-secret reddit/get reddit/post reddit/thing})
+(module-export! '{reddit/creds reddit.creds
+		  reddit/get reddit/post reddit/thing})
 
-(define reddit-key #f)
-(varconfig! reddit:key reddit-key)
-(define reddit-secret #f)
-(varconfig! reddit:secret reddit-secret)
+(define reddit.oauth #f)
+(define reddit.creds #f)
+
+(define reddit:keep {})
+(varconfig! reddit:keep reddit:keep)
+(define reddit:drop {})
+(varconfig! reddit:drop reddit:drop)
+
+(define (reddit/creds)
+  (or (and reddit.creds
+	   (future? (getopt reddit.creds 'expires))
+	   reddit.creds)
+      (let ((spec (if reddit.oauth
+		      (if (pair? reddit.oauth)
+			  reddit.oauth
+			  (cons reddit.oauth (oauth/provider 'reddit)))
+		      (oauth/provider 'reddit)))
+	    (creds (oauth/getclient spec)))
+	(set! reddit.creds creds)
+	creds)))
 
 (module-export! '{subreddits/search subreddits/new subreddits/popular
 		  subreddits/default subreddits/gold
@@ -59,17 +76,26 @@
 	   (for-choices (child (elts (get json 'children)))
 	     (reddit/thing child pool))
 	   'before (get json 'before) 'after (get json 'after)))
-	(else (let ((output (frame-create pool 'name (get json 'name))))
+	(else (let ((name (get json 'name))
+		    (created (->exact (get json 'created)))
+		    (created_utc (->exact (get json 'created_utc)))
+		    (output (frame-create pool
+			      'reddid name '%id (get json 'name))))
+		(store! output 'created
+			(mktime created_utc 'gmtoff (- created created_utc)))
 		(do-choices (slot (getkeys json))
 		  (let ((value (get json slot)))
-		    (cond ((or (fail? value)
-			       (overlaps? value {#f "" #()})
-			       (and (table? value) (fail? (getkeys value)))))
+		    (cond ((and (not (overlaps? slot reddit:keep))
+				(or (fail? value)
+				    (zero? value)
+				    (overlaps? slot reddit:drop)
+				    (overlaps? value {#f "" #()})
+				    (and (table? value) (fail? (getkeys value))))))
 			  ((string? value)
 			   (store! output slot (decode-entities (get json slot))))
-			  ((overlaps? slot '{created created_utc})
-			   (unless (timestamp? value)
-			     (store! output slot (timestamp (->exact value)))))
+			  ((overlaps? slot '{created created_utc}))
 			  (else (store! output slot (reddit/thing value))))))
 		output))))
+
+
 
