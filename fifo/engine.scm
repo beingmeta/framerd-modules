2b2fe94ce33652778d04e7486d278bc62893d4ba
Ken Haase <kh@beingmeta.com>
2017-10-01 19:53:05 -0400

    Improvements to engine implementation

--- fifo/engine.scm
+++ fifo/engine.scm
@@ -9,12 +9,16 @@
 
 (module-export! '{fifo/engine})
 
-(defambda (batchup items batchsize nthreads)
+;;; This divides a big set (choice) of items into smaller buckets.
+;;; The buckets (except for the last one) are all some multiple of
+;;; *batchsize* items; the multiple is in the range [1,N].
+;;; It returns a vector of those batches.
+(defambda (batchup items batchsize batchrange)
   (let ((n (choice-size items))
 	(batchlist '())
 	(start 0))
     (while (< start n)
-      (let ((step (* (1+ (random nthreads)) batchsize)))
+      (let ((step (* (1+ (random batchrange)) batchsize)))
 	(set! batchlist (cons (qc (pick-n items step start)) batchlist))
 	(set! start (+ start step))))
     (reverse (->vector batchlist))))
@@ -30,26 +34,40 @@
 	      (+ (getopt loop-state 'vtime 0.0) thread-time)))))
 
 (define (batch-step fcn fifo opts loop-state state
-		    beforefn afterfn progressfn)
+		    beforefn afterfn progressfn stopfn)
   (let* ((batch (fifo/pop fifo))
 	 (start (elapsed-time)))
     (while (and (exists? batch) batch)
       (loginfo |GotBatch| "Processing batch of " ($num (choice-size batch)) " items")
-      (when beforefn
-	(if state (beforefn (qc batch) state)  (beforefn (qc batch))))
+      (when beforefn (beforefn (qc batch) loop-state))
       (do-choices (item batch) (fcn item))
-      (when afterfn
-	(if state (afterfn (qc batch) state) (afterfn (qc batch))))
+      (when afterfn (afterfn (qc batch) loop-state))
       (bump-loop-state loop-state (choice-size batch) (elapsed-time start))
       (when progressfn
-	(if state
-	    (progressfn (choice-size batch) (elapsed-time start) state)
-	    (progressfn (choice-size batch) (elapsed-time start))))
-      (set! batch (fifo/pop fifo))
-      (set! start (elapsed-time)))))
+	(progressfn (qc batch) (elapsed-time start) loop-state))
+      (if (and stopfn (stopfn loop-state))
+	  (set! batch {})
+	  (let ((monitors (getopt opts 'monitors {})))
+	    (do-choices (monitor monitors)
+	      (cond ((applicable? monitor) (monitor loop-state))
+		    ((and (pair? monitor) (applicable? (car monitor)))
+		     (let ((testval ((car monitor) loop-state)))
+		       (when testval
+			 (do-choices (action (if (pair? (cdr monitor))
+						 (cadr monitor)
+						 (cdr monitor)))
+			   (if (applicable? action)
+			       (action testval loop-state)
+			       (logwarn |BadMonitorAction| action))))))
+		    (else (logwarn |BadMonitor| monitor))))
+	    (if (getopt loop-state 'stopped)
+		(set! batch {})
+		(begin
+		  (set! batch (fifo/pop fifo))
+		  (set! start (elapsed-time)))))))))
 
 (defambda (fifo/engine fcn items (opts #f))
-  (let* ((batchsize (getopt opts 'batchsize (config 'batchsize 10000)))
+  (let* ((batchsize (getopt opts 'batchsize (config 'batchsize 1024)))
 	 (nthreads (mt/threadcount (getopt opts 'nthreads (config 'nthreads #t))))
 	 (spacing (getopt opts 'spacing 
 			  (and nthreads (> nthreads 1)
@@ -59,35 +77,42 @@
 	 (before (getopt opts 'before #f))
 	 (after (getopt opts 'after #f))
 	 (progress (getopt opts 'progress #f))
+	 (stop (getopt opts 'stopfn #f))
+	 (state (getopt opts 'state #f))
 	 (loop-state (frame-create #f
+		       'fifo fifo
 		       'started (elapsed-time)
 		       'total (choice-size items)
 		       'n-batches (length batches)
-		       'monitors (getopt opts 'monitors {})))
-	 (state (getopt opts 'state #f))
+		       'monitors (getopt opts 'monitors {})
+		       'state state
+		       'opts opts))
 	 (threads {})
 	 (count 0))
     (when (and nthreads (> nthreads (length batches)))
       (set! nthreads (length batches)))
-    (when state (store! state 'loop loop-state))
     (lognotice |FIFOEngine| 
       "Processing " ($count (choice-size items)) " items "
-      "in " ($count (length batches)) " batches using " nthreads " threads "
+      "in " ($count (length batches)) " batches " 
+      "of up to " nthreads " ❌ " batchsize " items "
+      "using " nthreads " threads "
       "with\n   " fcn)
     (dotimes (i nthreads)
       (set+! threads 
 	(thread/call batch-step 
 	    fcn fifo opts 
 	    loop-state state 
-	    before after progress)
+	    before after progress stop)
 	(when spacing (sleep spacing))))
     (thread/wait threads)
     (commit)))
 
 (define (engine/fetchoids oids (state #f))
   (prefetch-oids! oids))
-(define (engine/fetchoids oids (state #f))
+(define (engine/fetchkeys oids (state #f))
   (prefetch-keys! oids))
+(define (engine/swapout args (state #f))
+  (swapout args))
 
 (define (engine/poolfetch pool)
   (ambda (oids (state #f)) (pool-prefetch! pool oids)))
@@ -95,36 +120,61 @@
 (define (engine/indexfetch index)
   (ambda (keys (state #f)) (prefetch-keys! index keys)))
 
-(define (engine/swapout arg)
-  (ambda (keys (state #f)) (swapout arg)))
-
 (define (engine/savepool pool)
-  (ambda (keys (state #f)) (commit pool)))
+  (ambda (keys (loop-state #f)) (commit pool)))
 
 (define (engine/saveindex index)
-  (ambda (keys (state #f)) (commit index)))
+  (ambda (keys (loop-state #f)) (commit index)))
 
 (define (engine/savetable table file)
-  (ambda (keys (state #f)) (dtype->file table file)))
+  (ambda (keys (loop-state #f)) (dtype->file table file)))
 
 (define (engine/interval interval)
-  (let ((last (elapsed-time)))
-    (slambda ((loop-state #f) (state #f))
-      (cond ((> (elapsed-time last) interval)
-	     (set! last (elapsed-time))
-	     #t)
-	    (else #f)))))
+  (slambda ((loop-state #f))
+    (cond ((> (elapsed-time last) interval)
+	   (set! last (elapsed-time))
+	   #t)
+	  (else #f))))
+
+(define (engine/maxitems max-count)
+  (slambda ((loop-state #f))
+    (cond ((> (getopt loop-state 'count 0) max-count)
+	   #t)
+	  (else #f))))
 
 (define (engine/memgrowth delta)
   (let ((last (memusage)))
-    (slambda ((loop-state #f) (state #f)) 
+    (slambda ((loop-state #f)) 
       (cond ((> (- (memusage) last) delta)
 	     (set! last (memusage))
 	     #t)
 	    (else #f)))))
 
-(module-export! '{engine/interval engine/memgrowth
+(define (optconf opts prop (dflt #f) (use-config))
+  (default! use-config
+    (getopt opts 'useconfig (not (getopt opts 'noconfig #f))))
+  (if use-config
+      (getopt opts prop (and use-config (config prop dflt)))
+      (getopt opts prop dflt)))
+
+(define (engine/stopfn (opts #f))
+  (let ((maxtime (getopt opts 'maxtime #f))
+	(maxcount (getopt opts 'maxcount #f))
+	(maxmem (getopt opts 'maxmem #f))
+	(maxload (getopt opts 'maxload #f)))
+    (lambda (loop-state)
+      (or (test loop-state 'stopped)
+	  (and (or (and maxtime 
+			(> (elapsed-time (get loop-state 'started))
+			   maxtime))
+		   (and maxcount (> (get loop-state 'count) maxcount))
+		   (and maxload (> (rusage 'load) maxload)))
+	       (begin (store! loop-state 'stopped)
+		 #t))))))
+
+(module-export! '{engine/interval engine/memgrowth engine/stopfn
 		  engine/fetchoids engine/fetchkeys 
 		  engine/poolfetch engine/indexfetch engine/swapout 
-		  engine/savepool engine/saveindex engine/savetable})
+		  engine/savepool engine/saveindex engine/savetable
+		  engine/maxitems})
 
