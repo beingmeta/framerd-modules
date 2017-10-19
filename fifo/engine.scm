7b85ab9cf3cd67bf11579820c2894862a273afd3
Ken Haase <kh@beingmeta.com>
2017-10-19 10:49:24 +0000

    Refinements to fifo/engine, including more detailed comments

--- fifo/engine.scm
+++ fifo/engine.scm
@@ -12,6 +12,67 @@
 (define engine-log-frequency 60)
 (varconfig! engine:logfreq engine-log-frequency)
 
+;;; This is a task loop engine where a task applies a function to a
+;;;  bunch of data items organized into batches.
+
+;;; Currently, this engine supports thread parallelism but not job
+;;; (process) parallelism. There are three levels:
+
+;;; 1. Task (outermost, cross-process)
+;;; 2. Loop (single process, single invocation of fifo/engine, often one invocation per process)
+;;; 3. Batch (single thread, set of items)
+
+;;; While running, a table (slotmap) is associated with each of the
+;;; levels, called *state*, *loop-state*, and *batch_state*
+;;; respectively.
+
+
+;;; The engine uses a FIFO of batches and a pool of threads pulling
+;;; batches from the FIFO and processing them. For a single batch,
+;;; there can be serveral phases (this is implemented by BATCH-STEP).
+
+#|Markdown
+
+1. BEFOREFN is called on the batch, the batch-state, the loop-state, and the
+   task state)
+
+2. PROCESSFN is the item function itself. It is called on each element
+of the batch. If it takes two arguments, it is called on the item and
+the batch state. If it takes 4 arguments, it is called on the item,
+batch state, loop state, and task state.
+
+3. AFTERFN is called on the batch, the batch-state, the loop-state, and the
+   task state)
+
+4. The 'loop state' is then 'bumped'. This updates the total
+'count' (number of items processed), total 'batches' (number of
+batches processed), and 'vtime' (how much realtime this thread spent
+on the batch).
+
+5. PROGRESSFN is then called on:
+ * the items processed;
+ * the clock time spent on PROCESS
+ * the clock time spent on BEFORE+PROCESS+AFTER
+ * the batch state
+ * the loop state
+ * the task state
+
+5. The STOPFNs are then called and if any return non-false, the loop
+'stops'. This means that the current thread stops processing batches
+and all of the other threads stop when they're done with their current
+batch.
+
+6. If the loop hasn't stopped, the loop's *monitors* are called. A
+monitor is either a function or a pair of functions. In the first
+case, the function is simply called on the loop-state; in the second,
+the CAR is called on the loop-state and the CDR is called if the CAR
+returns #t.
+
+The monitors can stop the loop by storing a value in the 'stopped
+slot of the loop state.
+
+|#
+
 ;;; This divides a big set (choice) of items into smaller buckets.
 ;;; The buckets (except for the last one) are all some multiple of
 ;;; *batchsize* items; the multiple is in the range [1,N].
@@ -28,49 +89,81 @@
 
 ;;; We make this handler unique right now, but it could be engine specific.
 (define-init bump-loop-state
-  (slambda (loop-state (count #f) (thread-time #f))
+  (slambda (loop-state (count #f) (proc-time #f) (thread-time #f))
     (store! loop-state 'batches (1+ (getopt loop-state 'batches 0)))
     (when count
       (store! loop-state 'count 
 	      (+ (getopt loop-state 'count 0) count)))
+    (when proc-time
+      (store! loop-state 'vproctime 
+	      (+ (getopt loop-state 'vproctime 0.0) proc-time)))
     (when thread-time
       (store! loop-state 'vtime 
 	      (+ (getopt loop-state 'vtime 0.0) thread-time)))))
 
-(define (batch-step fcn fifo opts loop-state state
-		    beforefn afterfn progressfn stopfn)
+(define (batch-step iterfn fifo opts loop-state state
+		    beforefn afterfn progressfn
+		    stopfn monitors)
   (let* ((batch (fifo/pop fifo))
-	 (start (elapsed-time)))
+	 (batch-state `#[loop ,loop-state started ,(elapsed-time) batchno 1])
+	 (start (get batch-state 'started))
+	 (proc-time #f)
+	 (batchno 1))
     (while (and (exists? batch) batch)
       (loginfo |GotBatch| "Processing batch of " ($num (choice-size batch)) " items")
-      (when beforefn (beforefn (qc batch) loop-state))
-      (do-choices (item batch) (fcn item))
-      (when afterfn (afterfn (qc batch) loop-state))
-      (bump-loop-state loop-state (choice-size batch) (elapsed-time start))
-      (when progressfn
-	(if (= (procedure-arity progressfn) 1)
-	    (progressfn loop-state)
-	    (progressfn (qc batch) (elapsed-time start) loop-state)))
-      (if (and stopfn (stopfn loop-state))
-	  (set! batch {})
-	  (let ((monitors (getopt opts 'monitors {})))
-	    (do-choices (monitor monitors)
-	      (cond ((applicable? monitor) (monitor loop-state))
-		    ((and (pair? monitor) (applicable? (car monitor)))
-		     (let ((testval ((car monitor) loop-state)))
-		       (when testval
-			 (do-choices (action (if (pair? (cdr monitor))
-						 (cadr monitor)
-						 (cdr monitor)))
-			   (if (applicable? action)
-			       (action testval loop-state)
-			       (logwarn |BadMonitorAction| action))))))
-		    (else (logwarn |BadMonitor| monitor))))
-	    (if (getopt loop-state 'stopped)
-		(set! batch {})
-		(begin
-		  (set! batch (fifo/pop fifo))
-		  (set! start (elapsed-time)))))))))
+      (when (and (exists? beforefn) beforefn)
+	(beforefn (qc batch) batch-state loop-state state))
+      (set! proc-time (elapsed-time))
+      (cond ((= (procedure-arity iterfn) 1)
+	     (do-choices (item batch) (iterfn item)))
+	    ((= (procedure-arity iterfn) 2)
+	     (do-choices (item batch) (iterfn item batch-state)))
+	    ((= (procedure-arity iterfn) 4)
+	     (do-choices (item batch) (iterfn item batch-state loop-state state)))
+	    (else (do-choices (item batch) (iterfn item))))
+      (set! proc-time (elapsed-time proc-time))
+      (when (and  (exists? afterfn) afterfn)
+	(afterfn (qc batch) batch-state loop-state state))
+      (bump-loop-state loop-state (choice-size batch) proc-time (elapsed-time start))
+      (cond ((not progressfn))
+	    ((not (applicable? progressfn))
+	     (engine/progress (qc batch) proc-time (elapsed-time start)
+			      batch-state loop-state state))
+	    ((= (procedure-arity progressfn) 1)
+	     (progressfn loop-state))
+	    ((= (procedure-arity progressfn) 3)
+	     (progressfn batch-state loop-state state))
+	    ((= (procedure-arity progressfn) 6)
+	     (progressfn (qc batch) proc-time (elapsed-time start)
+			 batch-state loop-state state)))
+      ;; Free some stuff up, maybe
+      (set! batch {})
+      (let ((stopval (or (test loop-state 'stopped)
+			(and (exists? stopfn) stopfn (exists stopfn loop-state)))))
+	(cond ((and (exists? stopval) stopval)
+	       (store! loop-state 'stopped (timestamp))
+	       (store! loop-state 'stopval stopval))
+	      (monitors
+	       (do-choices (monitor monitors)
+		 (cond ((applicable? monitor) (monitor loop-state))
+		       ((and (pair? monitor) (applicable? (car monitor)))
+			(let ((testval ((car monitor) loop-state)))
+			  (when testval
+			    (do-choices (action (if (pair? (cdr monitor))
+						    (cadr monitor)
+						    (cdr monitor)))
+			      (if (applicable? action)
+				  (action testval loop-state)
+				  (logwarn |BadMonitorAction| action))))))
+		       (else (logwarn |BadMonitor| monitor))))))
+	(if (getopt loop-state 'stopped)
+	    (set! batch {})
+	    (begin
+	      (set! batch (fifo/pop fifo))
+	      (set! batchno (1+ batchno))
+	      (set! start (elapsed-time))
+	      (set! batch-state
+		`#[loop ,loop-state started ,start batchno ,batchno])))))))
 
 (defambda (fifo/engine fcn items (opts #f))
   (let* ((batchsize (getopt opts 'batchsize (config 'batchsize 1024)))
@@ -99,21 +192,22 @@
 	 (threads {})
 	 (count 0))
     
-    (unless (and (applicable? fcn)
-		 (= (procedure-arity fcn) 1))
-      (irritant fcn |ENGINE/InvalidLoopFn| fifo/engine))
-    (when progress
-      (unless (and (applicable? progress)
-		   (or (= (procedure-arity progress) 1)
-		       (= (procedure-arity progress) 3)))
-	(irritant progress |ENGINE/InvalidProgressFn| fifo/engine)))
-    (when before
-      (unless (and (applicable? before) (= (procedure-arity before) 2))
-	(irritant before |ENGINE/InvalidBeforeFn| fifo/engine)))
-    (when after
-      (unless (and (applicable? after) (= (procedure-arity after) 2))
-	(irritant before |ENGINE/InvalidBeforeFn| fifo/engine)))
-
+    (do-choices fcn
+      (unless (and (applicable? fcn) (overlaps? (procedure-arity fcn) {1 2 4}))
+	(irritant fcn |ENGINE/InvalidLoopFn| fifo/engine)))
+    (when (and (exists? progress) progress)
+      (do-choices (progressfn (difference progress #t))
+	(unless (and (applicable? progressfn) (overlaps? (procedure-arity progressfn) {1 3 6}))
+	  (irritant progress |ENGINE/InvalidProgressFn| fifo/engine))))
+    (when (and (exists? before) before)
+      (do-choices before
+	(when before
+	  (unless (and (applicable? before) (= (procedure-arity before) 4))
+	    (irritant before |ENGINE/InvalidBeforeFn| fifo/engine)))))
+    (when (and (exists? after) after)
+      (do-choices after
+	(unless (and (applicable? after) (= (procedure-arity after) 4))
+	  (irritant after |ENGINE/InvalidAfterFn| fifo/engine))))
     (lognotice |FIFOEngine| 
       "Processing " ($count (choice-size items)) " items "
       (when (and batchsize (> batchsize 1))
@@ -126,7 +220,8 @@
 	(thread/call batch-step 
 	    fcn fifo opts 
 	    loop-state state 
-	    before after progress stop)
+	    before after progress
+	    stop (getopt opts 'monitors))
 	(when spacing (sleep spacing))))
     (thread/wait threads)
     (if (getopt opts 'skipcommit)
@@ -137,18 +232,46 @@
 
 ;;;; Utility functions
 
-(define (engine/fetchoids oids (state #f))
+(define (engine/fetchoids oids (batch-state #f) (loop-state #f) (state #f))
   (prefetch-oids! oids))
-(define (engine/fetchkeys oids (state #f))
+(define (engine/fetchkeys oids (batch-state #f) (loop-state #f) (state #f))
   (prefetch-keys! oids))
-(define (engine/swapout args (state #f))
+(define (engine/swapout args (batch-state #f) (loop-state #f) (state #f))
   (swapout args))
 
-(define (engine/progress loop-state)
-  (lognotice |Progress| 
-    "Processed " ($num (get loop-state 'count)) " items in " 
-    ($num (get loop-state 'batches)) " after "
-    (secs->string (elapsed-time (get loop-state 'started)))))
+(define (engine/progress batch proctime time batch-state loop-state state)
+  (let ((count (get loop-state 'count))
+	(loopmax (getopt loop-state 'total))
+	(total (getopt state 'total))
+	(taskmax (getopt state 'total))
+	(%loglevel (getopt loop-state '%loglevel %loglevel)))
+    (loginfo |Batch/Progress| 
+      "Processed " ($num (choice-size batch)) " items in " 
+      (secs->string (elapsed-time (get batch-state 'started))) " or ~"
+      ($num (round (/~ (choice-size batch) (elapsed-time (get batch-state 'started)))))
+      " items/second for this batch and thread.")
+    (lognotice |Engine/Progress| 
+      "Processed " ($num (get loop-state 'count)) " items" 
+      (when loopmax
+	(printout " (" (show% (get loop-state 'count) loopmax) " of "
+	  ($num loopmax) ")"))
+      " in " (secs->string (elapsed-time (get loop-state 'started))) ". "
+      "Averaging " 
+      ($num (round (/~ (get loop-state 'count) (elapsed-time (get loop-state 'started)))))
+      " items/second in this loop. "
+      (when loopmax
+	(let* ((rate (/~ (get loop-state 'count) (elapsed-time )))
+	       (togo (- loopmax count))
+	       (timeleft (/~ togo rate))
+	       (finished (timestamp+ timeleft)))
+	  (printout "\nAt this range, the loop's " ($num loopmax) " items should be finished in "
+	    "~" (secs->string timeleft) " (~"
+	    (get finished 'timestring) " "
+	    (cond ((equal? (get (timestamp) 'datestring) (get finished 'datestring)))
+		  ((< (difftime finished) (* 24 3600)) "tomorrow")
+		  ((< (difftime finished) (* 24 4 3600)) (get finished 'weekday-long))
+		  (else (get finished 'rfc822date)))
+	    ")"))))))
 
 (module-export! '{engine/fetchoids engine/fetchkeys engine/progress})
 
