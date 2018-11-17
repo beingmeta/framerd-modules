cbd6fe3a4fc3e3c5963874a6a1d2e5ae394f7364
Ken Haase <kh@beingmeta.com>
2018-11-17 02:34:36 +0000

    Remove mongodb, now in libscm

--- mongodb/indexes.scm
+++
@@ -1,7 +0,0 @@
-;;; -*- Mode: Scheme; Character-encoding: utf-8; -*-
-;;; Copyright (C) 2005-2018 beingmeta, inc.  All rights reserved.
-
-(in-module 'mongodb/indexes)
-
-(use-module '{mongodb logger})
-
