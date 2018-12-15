680322168e5d042b05dfa1410cedb959d7946678
Ken Haase <kh@beingmeta.com>
2018-12-15 16:35:41 +0000

    Update bench, also moved to libscm

--- bench/bench.scm
+++ bench/bench.scm
@@ -9,19 +9,21 @@
 (varconfig! repeat default-repeat)
 
 (define (benchn repeat benchmark-name fn . args)
+  (when (applicable? benchmark-name)
+    (set! args (cons fn args))
+    (set! fn benchmark-name)
+    (set! benchmark-name 
+      (or (procedure-name benchmark-name) benchmark-name)))
   (let ((start (timestamp)))
     (dotimes (i repeat) (apply fn args))
     (let ((time (difftime (timestamp) start)))
       (logwarn |Benchmark| benchmark-name 
 	" (" repeat " cycles) took " time " seconds"))))
 
-(define (bench benchmark-name fn . args)
-  (let ((repeat (config 'repeat 5))
-	(start (timestamp)))
-    (dotimes (i default-repeat) (apply fn args))
-    (let ((time (difftime (timestamp) start)))
-      (logwarn |Benchmark| benchmark-name 
-	" (" repeat " cycles) took " time " seconds"))))
+(define (bench benchmark-name . args)
+  (if (number? benchmark-name)
+      (apply benchn benchmark-name args)
+      (apply benchn default-repeat benchmark-name args)))
 
 (module-export! '{bench benchn})
 
