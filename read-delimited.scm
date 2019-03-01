7a34202fe58f1fcd0eb6011610ec738be86ecb25
Rommel MARTINEZ <ebzzry@ebzzry.io>
2019-03-01 19:37:02 +0800

    read-delimited.scm: refactor composition

--- read-delimited.scm
+++ read-delimited.scm
@@ -36,8 +36,6 @@
 (define (read-delimited-file file (cols #f) (cellsep ","))
   (read-delimited (filestring file) cols cellsep))
 
-(define (read-delimited-line line) #f)
-
 ;;; Return a list from FILE
 (define (file->list file)
   (string-split (filestring file) #\newline))
@@ -50,7 +48,7 @@
 
 ;;; Return cleaned up list from FILE
 (define (file->list/clean file)
-  (uncomment (clean-up (file->list file))))
+  (uncomment (file->list file)))
 
 ;;; Append newline to string
 (define (string-append-newline string)
@@ -62,3 +60,50 @@
     (apply string-append
            (map string-append-newline
                 (file->list/clean file)))))
+
+;;; Return a list containing sublists of string items in CONTENT
+(define (split-content content (delimiter #\,))
+  (map (lambda (line) (string-split line delimiter))
+       content))
+
+;;; Make pairs
+(define (*make-pairs x y acc)
+  (cond ((or (null? x) (null? y)) acc)
+        (else (*make-pairs (rest x)
+                           (rest y)
+                           (append acc (list (first x) (first y)))))))
+
+(define (make-pairs x y)
+  (*make-pairs x y '()))
+
+;;; Make frames
+(define (make-frame x y (f #[]))
+  (cond ((or (null? x) (null? y)) f)
+        (else (begin
+                (store! f (first x) (first y))
+                (make-frame (rest x) (rest y) f)))))
+
+;;; Return true if the list is of even length
+(define (even-list? xs)
+  (even? (length xs)))
+
+;;; Return true if the list is of odd length
+(define (odd-list? xs)
+  (not (even-list? xs)))
+
+;;; Return a frame from list
+(define (list->frame xs)
+  (when (even-list? xs)
+    (let ((f (frame-create #f)))
+      (dotimes (i (length ))))))
+
+;;; Compose content
+(define (compose-content content)
+  (let ((head (first content))
+        (body (rest content)))
+    (map->choice (lambda (entry) (make-frame head entry))
+                 body)))
+
+;;; Compose file
+(define (compose-file file (delimiter #\,))
+  (compose-content (split-content (file->list/clean file) delimiter)))
