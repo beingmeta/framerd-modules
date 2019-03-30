014535aef138f2661efbe908ec899d66d52fa191
Ken Haase <kh@beingmeta.com>
2019-03-30 14:31:04 +0000

    Fix old storage/flex reference

--- slotindex.scm
+++ slotindex.scm
@@ -9,7 +9,7 @@
 
 (in-module 'slotindex)
 
-(use-module '{logger varconfig storage/flex})
+(use-module '{logger varconfig flexdb})
 
 (module-export! '{slotindex/make slotindex/setup 
 		  slotindex/init slotindex/add!
